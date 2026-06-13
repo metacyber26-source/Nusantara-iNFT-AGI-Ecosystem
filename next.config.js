b9eee41ae2a11ea67895654cc3dec32c3e819ba2
metacyber26-source <metacyber26@gmail.com>
2026-06-14 06:52:10 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.pinet.com https://sandbox.pinet.com http://sandbox.pinet.com;",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://*.pinet.com',
+          }
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
